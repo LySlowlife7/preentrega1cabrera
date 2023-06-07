89fb78cf5e7c74de3cad6ac0735e5e3972190a41
Lidia Cabrera <ly_judas@hotmail.com>
2023-06-07 17:05:08 -0300

    hice modificaciones agregue archivos jsn

--- main.js
+++ main.js
@@ -1,80 +1,68 @@
-const PRODUCTOS = [
-  { NOMBRE: "BUZO", PRECIO: 3000 },
-  { NOMBRE: "REMERAS", PRECIO: 1500 },
-  { NOMBRE: "JEAN", PRECIO: 5000 },
-  { NOMBRE: "ZAPATILLAS", PRECIO: 6000 },
-];
+document.addEventListener('DOMContentLoaded', () => {
+  const listaCarrito = document.getElementById('listaCarrito');
+  const totalCarrito = document.getElementById('totalCarrito');
+  const agregarCarritoButtons = document.querySelectorAll('.agregarCarrito');
+  const vaciarCarritoButton = document.getElementById('vaciarCarritoButton');
+  let carrito = [];
 
-alert("Ingrese la opción del producto que desea llevar, para salir ingrese 0");
+  new Vue({
+    el: '#productos',
+    data: {
+      productos: [],
+      filtroCategoria: ''
+    },
+    computed: {
+      productosFiltrados() {
+        if (this.filtroCategoria) {
+          return this.productos.filter(producto => producto.CATEGORIA === this.filtroCategoria);
+        }
+        return this.productos;
+      }
+    },
+    mounted() {
+      this.cargarProductos();
+    },
+    methods: {
+      cargarProductos() {
+        // Carga los productos desde el archivo JSON local
+        fetch('productos.json')
+          .then(response => response.json())
+          .then(data => {
+            this.productos = data;
+          })
+          .catch(error => {
+            console.error('Error al cargar los productos:', error);
+          });
+      },
+      agregarAlCarrito(producto) {
+        carrito.push(producto);
+        this.mostrarCarrito();
+      },
+      mostrarCarrito() {
+        listaCarrito.innerHTML = '';
+        let total = 0;
+        carrito.forEach((producto) => {
+          const { NOMBRE, PRECIO } = producto;
+          const itemCarrito = document.createElement('li');
+          itemCarrito.innerHTML = `${NOMBRE} - Precio: $${PRECIO}`;
+          listaCarrito.appendChild(itemCarrito);
+          total += PRECIO;
+        });
+        totalCarrito.textContent = `Total: $${total}`;
+      },
+      vaciarCarrito() {
+        carrito = [];
+        this.mostrarCarrito();
+      }
+    }
+  });
 
-let seleccionarProductos = Number(
-  prompt(
-    `1-${PRODUCTOS[0].NOMBRE} $${PRODUCTOS[0].PRECIO} 2-${
-      PRODUCTOS[1].NOMBRE
-    } $${PRODUCTOS[1].PRECIO} 3-${
-      PRODUCTOS[2].NOMBRE
-    } $${PRODUCTOS[2].PRECIO} 4-${
-      PRODUCTOS[3].NOMBRE
-    } $${PRODUCTOS[3].PRECIO}`
-  )
-);
-let seleccionarCantidad;
-let total = 0;
+  // seccion de Agregar evento al botón de "Vaciar Carrito"
+  vaciarCarritoButton.addEventListener('click', () => {
+    app.vaciarCarrito();
+  });
+});
 
-// seccion para calcular el valor de la compra de un producto
-const cantidad = (cant, precio) => {
-  return cant * precio;
-};
 
-while (seleccionarProductos !== 0) {
-  // Use map() para obtener un array con los precios de cada producto
-  const precios = PRODUCTOS.map(producto => producto.PRECIO);
-  const indice = seleccionarProductos - 1; // El índice en el array de precios es la opción seleccionada menos 1
-  
-  // Use filter() para buscar el producto seleccionado y obtener su precio
-  const precioSeleccionado = PRODUCTOS.filter(producto => producto === PRODUCTOS[indice])[0].PRECIO;
-  
-  seleccionarCantidad = Number(
-    prompt(`el producto seleccionado es ${PRODUCTOS[indice].NOMBRE}, indique la cantidad`)
-  );
-  total += cantidad(seleccionarCantidad, precioSeleccionado);
-  
-  seleccionarProductos = Number(
-    prompt(
-      `1-${PRODUCTOS[0].NOMBRE} $${PRODUCTOS[0].PRECIO} 2-${
-        PRODUCTOS[1].NOMBRE
-      } $${PRODUCTOS[1].PRECIO} 3-${
-        PRODUCTOS[2].NOMBRE
-      } $${PRODUCTOS[2].PRECIO} 4-${
-        PRODUCTOS[3].NOMBRE
-      } $${PRODUCTOS[3].PRECIO}`
-    )
-  );
-}
 
-alert(`el total de la compra es de: $${total}`);
 
-const ENVIO = () => {
-  if (total >= 10000) {
-    alert("El envío es gratuito");
-  } else {
-    total += 1000;
-    alert(`el costo de envío es de $1000, el total es: $${total}`);
-  }
-};
-
-// seccion de envios y metodo de pago()
-
-const METODO_DE_PAGO = () => {
-  let metodo = prompt("Ingrese el método de pago (efectivo o tarjeta):");
-  let totalConDescuento = total;
-  
-  if (metodo === "tarjeta") {
-    totalConDescuento *= 1.1; // Aplicamos un 10% de recargo por pago con tarjeta
-  }
-  
-  alert(`El total con el método de pago seleccionado es: $${totalConDescuento}`);
-};
-
-ENVIO();
-METODO_DE_PAGO();
